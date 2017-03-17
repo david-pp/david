b76397ac6a7f78f5f43f1a7560db6ddd25bc5cee
david++ <heaven.hell.or@gmail.com>
2017-03-17 11:05:26 +0800

    learn-cpp改名

---
+++ clion/cpp/construct.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <typeinfo>
+
+struct A {
+    A(int i) {}
+
+    A(int x, int y) {}
+};
+
+struct B : public A {
+    using A::A;
+};
+
+template<typename T>
+struct C : public A {
+    using A::A;
+};
+
+template<typename T>
+struct D : public T {
+    using T::T;
+};
+
+
+
+
+
+int main() {
+    B b(1);
+    C<int> c(1);
+
+    D<A> d(1);
+    D<A> d2(1, 2);
+
+    {
+        int *a, *b;
+        std::cout << typeid(a).name() << std::endl;
+        std::cout << typeid(b).name() << std::endl;
+    }
+}
--- clion/snippet/reflection.cpp
+++ clion/snippet/reflection.cpp
@@ -5,175 +5,28 @@
 #include <unordered_map>
 #include <typeinfo>
 #include <boost/any.hpp>
-
-template<typename T>
-class Property {
-public:
-    typedef std::shared_ptr<Property> Ptr;
-
-    Property(const std::string &name) : name_(name) {}
-
-    virtual const std::type_info &type() = 0;
-
-    virtual boost::any get(T &) = 0;
-
-    virtual void set(T &, boost::any &) = 0;
-
-    const std::string &name() { return name_; }
-
-private:
-    std::string name_;
-};
-
-
-template<typename T, typename MemFn>
-class Property_T : public Property<T> {
-public:
-    Property_T(const std::string &name, MemFn fn)
-            : Property<T>(name), fn_(fn) {}
-
-    virtual const std::type_info &type() final {
-        return typeid(decltype(fn_(T())));
-    };
-
-    boost::any get(T &obj) final {
-        return fn_(obj);
-    }
-
-    void set(T &obj, boost::any &v) final {
-        fn_(obj) = boost::any_cast<decltype(fn_(obj))>(v);
-    }
-
-
-private:
-    MemFn fn_;
-};
-
-template<typename T, typename MemFn>
-Property_T<T, MemFn> *makePropery(const std::string &name, MemFn fn) {
-    return new Property_T<T, MemFn>(name, fn);
-}
-
+#include <sstream>
+#include "reflection.h"
 
 ///////////////////////////////////////////////
 
 
-template<typename T>
-struct Struct {
-public:
-    typedef std::vector<typename Property<T>::Ptr> PropertyContainer;
-    typedef std::unordered_map<std::string, typename Property<T>::Ptr> PropertyMap;
-
-    Struct(const std::string &name) : name_(name) {}
-
-    T *clone() { return new T; }
-
-    template<typename PropType>
-    Struct<T> &property(const std::string &name, PropType T::* prop) {
-        if (!hasPropery(name)) {
-            typename Property<T>::Ptr ptr(makePropery<T>(name, std::mem_fn(prop)));
-            properties_[name] = ptr;
-            properties_ordered_.push_back(ptr);
-        }
-
-        return *this;
-    }
-
-    bool hasPropery(const std::string &name) {
-        return properties_.find(name) != properties_.end();
-    }
-
-    size_t propertyCount() { return properties_.size(); }
-
-    PropertyContainer propertyIterator() { return properties_ordered_; }
-
-    typename Property<T>::Ptr getPropertyByName(const std::string& name) {
-        auto it = properties_.find(name);
-        if (it != properties_.end())
-            return it->second;
-        return typename Property<T>::Ptr();
-    }
-
-private:
-    std::string name_;
-    PropertyContainer properties_ordered_;
-    PropertyMap properties_;
+struct PodType {
+    int a;
+    float b;
+    char c;
+    char name[20];
 };
-
-
-template<typename T>
-struct Reflection : public T {
-    static Struct<T> *descriptor;
-
-    template<typename PropType>
-    PropType get(const std::string &propname) {
-        auto prop = descriptor->getPropertyByName(propname);
-        if (prop)
-            return boost::any_cast<PropType>(prop->get(*this));
-        return PropType();
-    }
-
-    template<typename PropType>
-    void set(const std::string &propname, const PropType &value) {
-        auto prop = descriptor->getPropertyByName(propname);
-        if (prop) {
-            boost::any v = value;
-            prop->set(*this, v);
-        }
-    }
-
-    void dump() {
-        for (auto it : descriptor->propertyIterator()) {
-            if (it->type() == typeid(int))
-                std::cout << it->name()  << ":" << get<int>(it->name()) << std::endl;
-            else if (it->type() == typeid(std::string))
-                std::cout << it->name() << ":" << get<std::string>(it->name()) << std::endl;
-        }
-    }
-};
-
-template<typename T>
-Struct<T> *Reflection<T>::descriptor = NULL;
-
-struct StructFactory {
-    static StructFactory &instance() {
-        static StructFactory instance_;
-        return instance_;
-    }
-
-    template<typename T>
-    Struct<T> &declare() {
-        std::string name = typeid(T).name();
-        Struct<T> *desc = new Struct<T>(name);
-        Reflection<T>::descriptor = desc;
-        structs_[name] = desc;
-        return *desc;
-    }
-
-    template<typename T>
-    Struct<T> *classByType() {
-        std::string name = typeid(T).name();
-        if (structs_.find(name) != structs_.end())
-            return boost::any_cast<Struct<T> *>(structs_[name]);
-        return NULL;
-    }
-
-private:
-    typedef std::unordered_map<std::string, boost::any> Structs;
-
-    Structs structs_;
-};
-
-
-///////////////////////////////////////////////
-
-
 
 struct Player {
     int id = 0;
     std::string name;
     int score = 0;
+
+    std::vector<int> values;
+    PodType pod;
 };
+
 
 // in .cpp
 
@@ -182,12 +35,54 @@ struct Register {
         StructFactory::instance().declare<Player>()
                 .property("id", &Player::id)
                 .property("name", &Player::name)
-                .property("score", &Player::score);
+                .property("score", &Player::score)
+                .property("values", &Player::values);
 
     }
 };
 
 Register reg;
+
+template<typename T>
+std::string serialize(const T &object) {
+//    Struct<T>* reflection = StructFactory::instance().classByType<T>();
+//    if (reflection){
+//
+//    }
+    return std::string();
+}
+
+
+
+//template<>
+//struct Serialize<Player> {
+//    static std::string serialize(Player &p) {
+//        return Serialize<int>::serialize(p.id)
+//               + Serialize<std::string>::serialize(p.name)
+//               + Serialize<int>::serialize(p.score)
+//               + Serialize<std::vector<int>>::serialize(p.values)
+//               + Serialize<PodType>::serialize(p.pod);
+////        return std::to_string(p.id) + "," + p.name + "," + std::to_string(p.score);
+//    }
+//};
+
+void test_b() {
+
+    std::cout << " ------------- " << std::endl;
+
+
+    Player p;
+    p.id = 1024;
+    p.name = "david";
+    p.score = 100;
+    p.values.push_back(10);
+    p.values.push_back(20);
+    std::cout << Serialize<Player>::serialize(p) << std::endl;
+
+    std::vector<Player> players = {p, p, p};
+
+    std::cout << Serialize<std::vector<Player>>::serialize(players) << std::endl;
+}
 
 
 void test_r() {
@@ -205,7 +100,7 @@ void test_r() {
 
     for (auto it : Reflection<Player>::descriptor->propertyIterator()) {
         if (it->type() == typeid(int))
-            std::cout << it->name()  << ":" << p.get<int>(it->name()) << std::endl;
+            std::cout << it->name() << ":" << p.get<int>(it->name()) << std::endl;
         else if (it->type() == typeid(std::string))
             std::cout << it->name() << ":" << p.get<std::string>(it->name()) << std::endl;
     }
@@ -237,11 +132,44 @@ void test_n() {
 
     std::cout << typeid(v).name() << std::endl;
     std::cout << typeid(int).name() << std::endl;
+
+    auto get_id = std::mem_fn(&Player::id);
+
+    using GetType = decltype(get_id);
+    using IdType = std::remove_reference<decltype(get_id(std::declval<Player>()))>::type;
+    using IdType2 = std::remove_reference<std::result_of<decltype(get_id)(Player & )>::type>::type;
+
+    std::cout << get_id(p) << std::endl;
+    static_assert(std::is_same<IdType, int>::value, "aaaaaaaa");
+    static_assert(std::is_same<IdType, IdType2>::value, "bbbbbbbb");
 }
 
+
+struct Npc {
+    uint32_t id = 4729;
+    std::string name = "david";
+    uint16_t category = 10;
+};
+
+template<>
+struct Object2Bin<Npc> {
+    static bool from_bin(Npc &obj, const std::string &bin) {
+
+        return true;
+    }
+
+    static bool to_bin(Npc &obj, std::string &bin) {
+        std::ostringstream os;
+        os << obj.id << "," << obj.name << "," << obj.category;
+        bin = os.str();
+        return true;
+    }
+};
+
+
 int main() {
-//    test_n();
-//    test_1();
     test_r();
+    test_n();
+    test_b();
     return 0;
 }
