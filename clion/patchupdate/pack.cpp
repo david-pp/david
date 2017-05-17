0a17e99f186d4021868c7d294eaa08b653080981
david++ <heaven.hell.or@gmail.com>
2017-05-18 01:03:59 +0800

    patchupdate

--- clion/patchupdate/pack.cpp
+++ clion/patchupdate/pack.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstring>
 #include "zt.h"
 
 void test_des() {
     std::string value = "hello world!";
 
-    for (size_t i = 0; i < value.size(); i++)
-    {
-        std::cout << (int)value[i] << ",";
+    for (size_t i = 0; i < value.size(); i++) {
+        std::cout << (int) value[i] << ",";
     }
     std::cout << std::endl;
 
@@ -25,9 +25,8 @@ void test_des() {
 
     DES_encrypt3((unsigned int *) value.data(), &keys[0], &keys[1], &keys[2]);
 
-    for (size_t i = 0; i < value.size(); i++)
-    {
-        std::cout << (int)value[i] << ",";
+    for (size_t i = 0; i < value.size(); i++) {
+        std::cout << (int) value[i] << ",";
     }
     std::cout << std::endl;
 
@@ -59,26 +58,83 @@ struct alignas(1) PackHeader {
     }
 };
 
-void parse(std::ifstream &file) {
+struct alignas(1) FileInfoHeader {
+    uint32_t id;
+    uint32_t offset;
+    uint32_t size;
+    uint32_t interspace_size;
+    uint32_t crc;
+};
+
+
+struct FileInfo : public FileInfoHeader {
+    std::string name;
+
+    void dump() {
+        std::cout << "---- FILE:" << name << "-------" << std::endl;
+        std::cout << "id                :" << id << std::endl;
+        std::cout << "offset            :" << offset << std::endl;
+        std::cout << "size              :" << size << std::endl;
+        std::cout << "interspace_size   :" << interspace_size << std::endl;
+        std::cout << "crc               :" << std::hex << std::showbase << crc << std::endl;
+        std::cout << std::dec;
+    }
+};
+
+void decrypt_header(uint8_t *in, size_t len) {
+    uint8_t key1[8] = {242, 162, 55, 253, 109, 63, 221, 238};
+    uint8_t key2[8] = {29, 188, 85, 99, 53, 50, 85, 17};
+    uint8_t key3[8] = {109, 63, 221, 238, 29, 188, 85, 99};
+
+    DES_key_schedule keys[3];
+    DES_set_key((const_DES_cblock *) key1, &keys[0]);
+    DES_set_key((const_DES_cblock *) key2, &keys[1]);
+    DES_set_key((const_DES_cblock *) key3, &keys[2]);
+
+    for (size_t i = 0; i < len; i += 8) {
+        DES_decrypt3((unsigned int *) (in + i), &keys[0], &keys[1], &keys[2]);
+    }
+}
+
+void parse(std::ifstream &fs) {
     PackHeader header;
-    file.read(reinterpret_cast<char *>(&header), sizeof(header));
+    fs.read(reinterpret_cast<char *>(&header), sizeof(header));
     header.dump();
 
-    file.seekg(0, std::ios_base::end);
-    long size = file.tellg();
+    fs.seekg(0, std::ios_base::end);
+    long size = fs.tellg();
     std::cout << "filesize:" << size << std::endl;
 
-    file.seekg(size - header.headersize);
+    fs.seekg(size - header.headersize);
 
     std::vector<uint8_t> buf;
     buf.resize(header.headersize);
-    file.read(reinterpret_cast<char *> (buf.data()), buf.size());
+    fs.read(reinterpret_cast<char *> (buf.data()), buf.size());
+
+    decrypt_header(buf.data(), buf.size());
+
+//    hexdump(std::string((char *) buf.data(), buf.size()));
+
+    uint8_t *p = buf.data();
+    for (int i = 0; i < header.filenum; ++i) {
+        FileInfo fileinfo;
+        std::memcpy(&fileinfo, p, sizeof(FileInfoHeader));
+        p += sizeof(FileInfoHeader);
+
+        char name[1024] = "";
+        strncpy(name, (char*)p, sizeof(name));
+        p += strlen(name) + 1;
+
+        fileinfo.name = name;
+
+        fileinfo.dump();
+    }
 }
 
 
 int main(int argc, const char *argv[]) {
-    test_des();
-    return 0;
+//    test_des();
+//    return 0;
     if (argc < 2) {
         std::cerr << "Usage:" << argv[0] << " file" << std::endl;
         return EXIT_FAILURE;
--- clion/patchupdate/zt.h
+++ clion/patchupdate/zt.h
@@ -3,5 +3,41 @@
 
 #define DES_DEFAULT_OPTIONS
 #include "des.h"
+#include <string>
+#include <iomanip>
+#include <iostream>
+
+inline void hexdump(const std::string &hex, std::ostream &os = std::cout) {
+    size_t line = 0;
+    size_t left = hex.size();
+
+    while (left) {
+        size_t linesize = (left >= 16 ? 16 : left);
+        std::string linestr = hex.substr(line * 16, linesize);
+
+        os << std::hex << std::setw(8) << std::setfill('0') << line * 16 << " ";
+        for (size_t i = 0; i < linestr.size(); ++i) {
+            if (i % 8 == 0)
+                os << " ";
+
+            int v = (uint8_t) linestr[i];
+            os << std::hex << std::setw(2) << std::setfill('0') << v << " ";
+        }
+
+        os << " " << "|";
+        for (size_t i = 0; i < linestr.size(); ++i) {
+            if (std::isprint(linestr[i]))
+                os << linestr[i];
+            else
+                os << '.';
+        }
+        os << "|" << std::endl;
+
+        left -= linesize;
+        line++;
+    }
+
+    os << std::dec;
+}
 
 #endif //CLION_ZT_H
