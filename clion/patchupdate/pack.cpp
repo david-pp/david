c758f444365e18115611bb979c3f1a4b30a46142
david++ <heaven.hell.or@gmail.com>
2017-05-18 17:06:41 +0800

    patchupdate

--- clion/patchupdate/pack.cpp
+++ clion/patchupdate/pack.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <cstring>
+#include <boost/filesystem.hpp>
+#include <map>
 #include "zt.h"
 
 void test_des() {
@@ -96,6 +98,21 @@ void decrypt_header(uint8_t *in, size_t len) {
     }
 }
 
+void encrypt_header(uint8_t *in, size_t len) {
+    uint8_t key1[8] = {242, 162, 55, 253, 109, 63, 221, 238};
+    uint8_t key2[8] = {29, 188, 85, 99, 53, 50, 85, 17};
+    uint8_t key3[8] = {109, 63, 221, 238, 29, 188, 85, 99};
+
+    DES_key_schedule keys[3];
+    DES_set_key((const_DES_cblock *) key1, &keys[0]);
+    DES_set_key((const_DES_cblock *) key2, &keys[1]);
+    DES_set_key((const_DES_cblock *) key3, &keys[2]);
+
+    for (size_t i = 0; i < len; i += 8) {
+        DES_encrypt3((unsigned int *) (in + i), &keys[0], &keys[1], &keys[2]);
+    }
+}
+
 void parse(std::ifstream &fs) {
     PackHeader header;
     fs.read(reinterpret_cast<char *>(&header), sizeof(header));
@@ -115,6 +132,8 @@ void parse(std::ifstream &fs) {
 
 //    hexdump(std::string((char *) buf.data(), buf.size()));
 
+    std::vector<FileInfo> fileinfos;
+
     uint8_t *p = buf.data();
     for (int i = 0; i < header.filenum; ++i) {
         FileInfo fileinfo;
@@ -122,12 +141,133 @@ void parse(std::ifstream &fs) {
         p += sizeof(FileInfoHeader);
 
         char name[1024] = "";
-        strncpy(name, (char*)p, sizeof(name));
+        strncpy(name, (char *) p, sizeof(name));
         p += strlen(name) + 1;
 
         fileinfo.name = name;
 
         fileinfo.dump();
+
+        fileinfos.push_back(fileinfo);
+    }
+
+    for (auto &file : fileinfos) {
+        boost::filesystem::path p = file.name;
+//        std::cout << p.parent_path() << std::endl;
+        boost::filesystem::create_directories(p.parent_path());
+
+        std::string buf;
+        buf.resize(file.size);
+        fs.seekg(file.offset + sizeof(PackHeader));
+        fs.read(buf.data(), buf.size());
+
+        std::ofstream ofs(file.name, std::ios::binary);
+        ofs.write(buf.data(), buf.size());
+    }
+}
+
+void pack(std::ostream &os, const std::string &dir) {
+    using namespace boost::filesystem;
+
+    boost::filesystem::path p(dir);
+
+    try {
+        if (exists(p)) {
+            if (is_regular_file(p))
+                std::cout << p << " size is " << file_size(p) << '\n';
+
+            else if (is_directory(p)) {
+                std::cout << p << " is a directory containing:\n";
+
+
+                std::map<std::string, FileInfo> files;
+                for (directory_entry &x : directory_iterator(p)) {
+
+                    FileInfo fileinfo;
+                    fileinfo.name = x.path().string();
+                    fileinfo.id = zt2_crc32_zlib((unsigned char *) fileinfo.name.data(), fileinfo.name.length());
+                    fileinfo.size = file_size(x.path());
+                    fileinfo.interspace_size = fileinfo.size;
+                    fileinfo.crc = 0;
+                    fileinfo.offset = 0;
+                    files.insert(std::make_pair(fileinfo.name, fileinfo));
+//                    fileinfo.dump();
+                }
+
+                // write: pack header
+                PackHeader ph;
+                ph.magic = 0x4b415058;
+                ph.version = 9;
+                ph.filenum = files.size();
+                ph.flag = 0x2;
+                ph.headersize = 0;
+                // headsize
+                for (auto &pair : files) {
+                    ph.headersize += sizeof(FileInfoHeader);
+                    ph.headersize += pair.first.length() + 1;
+                }
+
+                // 8 bytes padding
+                ph.headersize = (ph.headersize + 7) & (~7);
+                ph.dump();
+
+                os.write((char *) &ph, sizeof(ph));
+
+                // write: files
+                uint32_t offset = 0;
+                for (auto &pair : files) {
+                    std::ifstream ifs(pair.second.name, std::ios::binary);
+                    pair.second.crc = zt_crc2(ifs);
+                    pair.second.offset = offset;
+                    pair.second.dump();
+
+                    std::string filedata;
+                    filedata.resize(pair.second.size);
+                    ifs.seekg(0);
+                    ifs.read(filedata.data(), pair.second.size);
+
+                    os.seekp(offset + sizeof(PackHeader));
+                    os.write(filedata.data(), filedata.size());
+
+                    offset += pair.second.size;
+                }
+
+                // write: header
+                uint32_t p = offset + sizeof(PackHeader);
+                os.seekp(p);
+
+                std::vector<uint8_t> head_buf;
+                head_buf.resize(ph.headersize);
+                uint32_t size = 0;
+                for (auto &pair : files) {
+
+                    memcpy(&head_buf[size], (char *) &pair.second, sizeof(FileInfoHeader));
+                    size += sizeof(FileInfoHeader);
+
+                    strcpy((char *) &head_buf[size], pair.first.c_str());
+                    size += pair.first.length();
+
+                    head_buf[size] = 0;
+                    size += 1;
+                }
+
+//                std::string hex;
+//                std::for_each(head_buf.begin(), head_buf.end(), [&hex](uint8_t& v)
+//                {
+//                    hex.push_back(v);
+//                });
+//
+//                hexdump(hex);
+
+                encrypt_header(head_buf.data(), head_buf.size());
+                os.write(head_buf.data(), head_buf.size());
+            } else
+                std::cout << p << " exists, but is not a regular file or directory\n";
+        } else
+            std::cout << p << " does not exist\n";
+    }
+    catch (const filesystem_error &ex) {
+        std::cout << ex.what() << '\n';
     }
 }
 
@@ -135,17 +275,30 @@ void parse(std::ifstream &fs) {
 int main(int argc, const char *argv[]) {
 //    test_des();
 //    return 0;
-    if (argc < 2) {
-        std::cerr << "Usage:" << argv[0] << " file" << std::endl;
+    if (argc < 3) {
+        std::cout << "Usage:" << std::endl;
+        std::cout << "\t" << argv[0] << " unpack file.pak" << std::endl;
+        std::cout << "\t" << argv[0] << " pack   file-dir  file-dir.pak" << std::endl;
         return EXIT_FAILURE;
     }
 
-    std::string filename = argv[1];
-    std::ifstream istrm(filename, std::ios::binary);
-    if (!istrm.is_open()) {
-        std::cout << "failed to open " << filename << '\n';
-    } else {
-        parse(istrm);
+    std::string op = argv[1];
+    if ("unpack" == op) {
+        std::string filename = argv[2];
+        std::ifstream istrm(filename, std::ios::binary);
+        if (!istrm.is_open()) {
+            std::cout << "failed to open " << filename << '\n';
+        } else {
+            parse(istrm);
+        }
+    } else if ("pack" == op) {
+        std::string dirname = argv[2];
+        if (argc > 3) {
+            std::ofstream ofs(argv[3], std::ios::binary);
+            pack(ofs, dirname);
+        } else {
+            pack(std::cout, dirname);
+        }
     }
 
     return EXIT_SUCCESS;
