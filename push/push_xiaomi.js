9529b8e4591525b7b6dd7d2b1347c11077c7a91c
david++ <heaven.hell.or@gmail.com>
2018-01-23 22:58:30 +0800

    push: ios title

--- push/push_xiaomi.js
+++ push/push_xiaomi.js
@@ -10,20 +10,35 @@ function push_by_device(device, title, content, platform, callback) {
   xiaomi_notify_id += 1;
 
   var key = 'key=pWR57EFBrRFgJHmg+oKQ5w=='
+  var formData = {}
   if (1 == platform)      // andriod
+  {
     key = 'key=nwSa9gFZB3MIWsqrRrsu9w=='
+
+    formData = {
+      title : title,
+      description : content,
+      registration_id : device,
+      restricted_package_name : 'com.ztgame.ztas',
+      notify_type : 2,
+      time_to_live : 3600000, 
+      notify_id : xiaomi_notify_id,
+    };
+  }
   else if (2 == platform) // ios
+  {
     key = 'key=pWR57EFBrRFgJHmg+oKQ5w=='
 
-  var formData = {
-    title : title,
-    description : content,
-    registration_id : device,
-    restricted_package_name : 'com.ztgame.ztas',
-    notify_type : 2,
-    time_to_live : 3600000, 
-    notify_id : xiaomi_notify_id,
-  };
+    formData = {
+      aps_proper_fields.title : title,
+      aps_proper_fields.body : content,
+      registration_id : device,
+      restricted_package_name : 'com.ztgame.ztas',
+      notify_type : 2,
+      time_to_live : 3600000, 
+      notify_id : xiaomi_notify_id,
+    };
+  }
 
   var options = {
     url: 'https://api.xmpush.xiaomi.com/v3/message/regid',
