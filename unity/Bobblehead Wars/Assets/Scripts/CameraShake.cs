51b4c4367d0a36712d024c15143cd2be65385977
david++ <397157852@qq.com>
2019-12-18 17:33:20 +0800

    《Unity By Tutorial 》- Chapter1-2

---
+++ unity/Bobblehead Wars/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+
+  public float shakeDecay = 0.002f;
+  public float intensity = .3f;
+
+  private Vector3 originPosition;
+  private Quaternion originRotation;
+  private float shakeIntensity = 0;
+
+  void Update() {
+    if (shakeIntensity > 0) {
+      transform.position = originPosition + Random.insideUnitSphere * shakeIntensity;
+      transform.rotation = new Quaternion(
+      originRotation.x + Random.Range(-shakeIntensity, shakeIntensity) * .2f,
+      originRotation.y + Random.Range(-shakeIntensity, shakeIntensity) * .2f,
+      originRotation.z + Random.Range(-shakeIntensity, shakeIntensity) * .2f,
+      originRotation.w + Random.Range(-shakeIntensity, shakeIntensity) * .2f);
+      shakeIntensity -= shakeDecay;
+    }
+  }
+
+  public void Shake() {
+    originPosition = transform.position;
+    originRotation = transform.rotation;
+    shakeIntensity = intensity;
+  }
+}
---
+++ unity/Bobblehead Wars/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	public int rotationSpeed;
+	
+	void Update () {
+		transform.Rotate(new Vector3(0, rotationSpeed, 0)* Time.deltaTime);
+	}
+}
