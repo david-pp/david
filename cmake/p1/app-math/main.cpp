baca43b509da4ecf26658c37e42bb13ec5267ff4
david++ <397157852@qq.com>
2020-11-18 17:26:51 +0800

    cmake - export find_package cmake file

---
+++ cmake/p1/app-math/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "tinymath.h"
+
+int main(int argc, const char** argv) {
+    std::cout << tinymath::square(10.0f) << std::endl;
+    return 0;
+}
+
--- cmake/p1/deps/libphysics/cmake/FindMath.cmake
+++ cmake/p1/deps/libphysics/cmake/FindMath.cmake
@@ -1,15 +1,18 @@
 
-message("try to find mathlib")
+
+# mathlib install path
+set(Math_INSTALL_PATH ${CMAKE_SOURCE_DIR}/../libmath/install)
+message("try to find mathlib : ${Math_INSTALL_PATH}")
 
 # find include
 find_path(Math_INCLUDE_DIR 
           NAMES tinymath.h
-          PATHS /Users/david/projects/david/cmake/p1/deps/libmath/build/tinymath-0.1.1-Darwin/include)
+          PATHS ${Math_INSTALL_PATH}/include)
 
 # find lib
 find_library(Math_LIBRARY
              NAMES tinymath 
-             PATHS /Users/david/projects/david/cmake/p1/deps/libmath/build/tinymath-0.1.1-Darwin/lib)
+             PATHS ${Math_INSTALL_PATH}/lib)
 
 # set directly
 # set(Math_INCLUDE_DIR /Users/david/projects/david/cmake/p1/deps/libmath/build/tinymath-0.1.1-Darwin/include)
@@ -17,6 +20,11 @@ find_library(Math_LIBRARY
 
 message("math-include: ${Math_INCLUDE_DIR}")
 message("math-lib    : ${Math_LIBRARY}")
+
+mark_as_advanced(
+    Math_INCLUDE_DIR
+    Math_LIBRARY
+)
 
 if (Math_INCLUDE_DIR AND Math_LIBRARY)
     set(Math_FOUND TRUE)
