a943dac2e78403dcb7f90e45c3dab95aaa9723a4
david++ <397157852@qq.com>
2020-11-19 19:43:33 +0800

    cmake

--- cmake/p1/app-math/main.cpp
+++ cmake/p1/app-math/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cmath>
 #include "tinymath.h"
 
 int main(int argc, const char** argv) {
-    std::cout << tinymath::square(10.0f) << std::endl;
+
+#ifdef USE_STD_MATH
+    std::cout << "stdmath =" << std::pow(10.0f, 2.0f) << std::endl;
+#else
+     std::cout << "tinymath =" << tinymath::square(10.0f) << std::endl;
+#endif
+
     return 0;
 }
 
